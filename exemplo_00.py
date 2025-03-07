86bb9a316a4a489bac926af55b340de4abd37b6f
VanthuirMaia <vanmaiasf@gmail.com>
2025-03-06 22:22:57 -0300

    feat - função

---
+++ exemplo_00.py
@@ -0,0 +1,17 @@
+valor_1 = 4
+valor_2 = 6
+
+
+valor_4 = 6
+valor_5 = 4
+
+
+
+def soma(valor_1: float, valor_2: float) -> float:
+    valor_3 = valor_1 + valor_2
+    return valor_3
+
+
+resultado = soma(valor_1, valor_2)
+
+print(resultado)
