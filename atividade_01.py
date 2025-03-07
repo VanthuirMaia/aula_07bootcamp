dfc56fd4964aadef2c272e2893608384a970e9ed
VanthuirMaia <vanmaiasf@gmail.com>
2025-03-06 22:29:53 -0300

    feat - atividade_01

---
+++ atividade_01.py
@@ -0,0 +1,10 @@
+# Crie uma função para calcular Média de Valores em uma Lista
+
+def caucular_media(list):
+    return sum(list) / len(list)
+
+lista = [1, 2, 5, 6, 8, 9, 4, 8, 5, 9]
+
+resultado = caucular_media(lista)
+
+print(resultado)
