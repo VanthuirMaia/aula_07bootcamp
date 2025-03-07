85693d7701f898536ef46d0c227c6a767083ce60
VanthuirMaia <vanmaiasf@gmail.com>
2025-03-06 22:36:21 -0300

    feat - atividade_03

---
+++ atividade_03.py
@@ -0,0 +1,9 @@
+# Crie uma função para Contar Valores Únicos em uma Lista
+
+def contar_valores(lista):
+    return len(set(lista))
+
+valores = [10, 20, 30, 10, 20, 40, 50, 30]
+quantidade_unicos = contar_valores(valores)
+
+print(f"Quantidade de valores únicos: {quantidade_unicos}")
